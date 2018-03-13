85307b7d69ef0798eab8cf328e655698f72fd7a7
Carlos - Sotreq <ica015@hotmail.com>
2018-03-13 17:24:52 -0300

    finalizacao do menu

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp'),
-    sass = require('gulp-sass');
+    sass = require('gulp-sass'),
+    browserSync = require('browser-sync');
 
 gulp.task('sass', function(){
     gulp.src('./src/sass/**/*.scss')
@@ -8,5 +9,15 @@ gulp.task('sass', function(){
 })
 
 gulp.task('listen', function(){
+    
+})
+
+gulp.task('server', function(){
+    browserSync.init({
+        server:{
+            baseDir:'src'
+        }
+    })
+    gulp.watch('./src/**/*').on('change', browserSync.reload)
     gulp.watch('./src/sass/**/*.scss',['sass'])
 })
